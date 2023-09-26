7f098f31553e794893896e227e8295f6d4554047
Shamo89 <145516284+Shamo89@users.noreply.github.com>
2023-09-25 22:44:23 -0600

    Create DataTypesEjercicio01.py

---
+++ DataTyepesEjercicio01.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+primer_digito = two_digit_number[0]
+segundo_digito = two_digit_number[1]
+resultado = int(primer_digito) + int(segundo_digito)
+print(resultado)
