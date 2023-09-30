d61bc148cc0b4b15b4b5c903b18ace5e680f20c8
Shamo89 <145516284+Shamo89@users.noreply.github.com>
2023-09-29 21:36:38 -0600

    Add files via upload

---
+++ CombinarFunciones.py
@@ -0,0 +1,11 @@
+#f-String = Al poner f despues del antes de " podemos combinar tipos de strings
+# Las diferentes funciones deben de colocarse dentro de {} para ser interpretadas correctamente
+
+
+score = 1+1
+score *= 5
+altura = 10.1
+
+ganando = True
+
+print(f"Tu puntuación es {score}, tu altura es {altura}, tu vas ganando es {ganando}")
