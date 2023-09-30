88648651acb8e2863989b29931254649bbb84819
Shamo89 <145516284+Shamo89@users.noreply.github.com>
2023-09-29 23:49:09 -0600

    Add files via upload

---
+++ CalculaPropina.py
@@ -0,0 +1,18 @@
+
+print("Bienvenido a la calculadora de propina.")
+
+cuenta = float(input("Cual es el total de la cuenta? $"))
+cantidad_propina = int(
+    input("Cuanto % quieres dejar de propina? 10, 12 o 15? "))
+personas = int(input("Entre cuantos quieres dividir la cuenta? "))
+
+porcentaje_propina = cantidad_propina / 100
+cuenta_con_propina = cuenta * porcentaje_propina + cuenta
+
+total = cuenta_con_propina / personas
+cantidad_total = round(total, 2)
+cantidad_total = "{:.2f}".format(total)
+
+print(f"Cada persona debe pagar ${cantidad_total} ")
+
+
