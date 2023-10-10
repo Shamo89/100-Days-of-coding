13116f333f16d02da2f255437e05020d1f0415b8
Shamo89 <145516284+Shamo89@users.noreply.github.com>
2023-10-09 21:05:20 -0600

    Add files via upload

---
+++ MontañaRusa.py
@@ -0,0 +1,8 @@
+print("Bienvenido a la montaña rusa!")
+estatura = int(input("Cual es tu estatura en centimetros? "))
+
+if estatura < 180:
+  print ("Excelente, puedes subir a la montaña rusa")
+
+else:   
+  print ("Lo siento U_U, no puedes subir a la montaña rusa")
