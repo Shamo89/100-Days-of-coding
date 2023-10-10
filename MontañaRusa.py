82dd506c322334b1ff45677631e04f411638fb91
Shamo89 <145516284+Shamo89@users.noreply.github.com>
2023-10-09 22:01:12 -0600

    Update MontañaRusa.py

--- MontañaRusa.py
+++ MontañaRusa.py
@@ -1,7 +1,7 @@
 print("Bienvenido a la montaña rusa!")
 estatura = int(input("Cual es tu estatura en centimetros? "))
 
-if estatura < 180:
+if estatura > 180:
   print ("Excelente, puedes subir a la montaña rusa")
 
 else:   
